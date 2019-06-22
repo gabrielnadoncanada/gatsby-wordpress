267fd021e22d2b85775e73d610775a228344e949
philwebdevelopper <51212970+philwebdevelopper@users.noreply.github.com>
2019-06-21 21:06:46 -0400

    Update accueil.js

--- gatsby-api/src/pages/accueil.js
+++ gatsby-api/src/pages/accueil.js
@@ -5,7 +5,7 @@ export default () => (
     <Layout>
         <h2>Accueil</h2>
         <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus faucibus est ac congue. In ultricies vehicula turpis, non fringilla mi consequat vel. Donec a turpis elementum, semper eros quis, lobortis ipsum. In in magna in magna egestas pharetra. Vestibulum volutpat, lorem et rutrum pellentesque, ante quam placerat velit, ut ultricies metus dolor in justo. Praesent ut lobortis nisi. Morbi semper hendrerit iaculis. Nunc commodo hendrerit mauris vel lobortis. Quisque eget metus ac lectus consequat finibus. Maecenas ac lacinia urna. Cras et enim commodo, mattis magna sit amet, imperdiet mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+            <p>Bienvenue dans le Task Manager ! Task Manager est une extension wordpress vous permettant de créer et de gérer vos tâches comme vous l'entendez !  </p>
         </div>
     </Layout>
 )
