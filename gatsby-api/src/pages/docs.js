608d76749b80b7e12ff5f601d57d5e54253d9f2e
philwebdevelopper <51212970+philwebdevelopper@users.noreply.github.com>
2019-06-21 22:09:10 -0400

    Update docs.js

--- gatsby-api/src/pages/docs.js
+++ gatsby-api/src/pages/docs.js
@@ -4,11 +4,9 @@ import Layout from "../components/layout"
 export default () => (
     <Layout>
         <h2>Docs</h2>
+        <h3>Guide d'utilisation</h3>
         <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus faucibus est ac congue. In ultricies vehicula turpis, non fringilla mi consequat vel. Donec a turpis elementum, semper eros quis, lobortis ipsum. In in magna in magna egestas pharetra. Vestibulum volutpat, lorem et rutrum pellentesque, ante quam placerat velit, ut ultricies metus dolor in justo. Praesent ut lobortis nisi. Morbi semper hendrerit iaculis. Nunc commodo hendrerit mauris vel lobortis. Quisque eget metus ac lectus consequat finibus. Maecenas ac lacinia urna. Cras et enim commodo, mattis magna sit amet, imperdiet mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        </div>
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus faucibus est ac congue. In ultricies vehicula turpis, non fringilla mi consequat vel. Donec a turpis elementum, semper eros quis, lobortis ipsum. In in magna in magna egestas pharetra. Vestibulum volutpat, lorem et rutrum pellentesque, ante quam placerat velit, ut ultricies metus dolor in justo. Praesent ut lobortis nisi. Morbi semper hendrerit iaculis. Nunc commodo hendrerit mauris vel lobortis. Quisque eget metus ac lectus consequat finibus. Maecenas ac lacinia urna. Cras et enim commodo, mattis magna sit amet, imperdiet mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+            <p>Pour l'ajout d'une nouvelle tâche, vous devez d'abord cliquer sur la section «Tasks» situé dans le menu principal en haut de page. Par la suite, vous allez tomber sur un formulaire comportant trois champs de saisie ainsi qu'un champ de sélection. C'est là que vous créez une nouvelle tâche. Attention, tous les champs de saisie ont besoin d'être préalablement rempli, sinon vous aurez un message d'erreur. <br><br> La nouvelle tâche crée comporte quatre paramètres : Un titre, une description, un destinataire ainsi que l'état de la tâche en cours. Normalement, une fois que vous aurez cliqué sur « submit », vous devriez voir apparaître la tâche juste en dessous du formulaire d'ajout de tâche, avec toutes ses informations. <br><br> Par la suite, vous avez le choix de modifier ou de supprimer la tâche, la mise à jour de la page courante se fera d'elle même une fois l'action envoyée. Tous les paramètres entrés lors de la création de la tâche sont modifiables en tout temps.     </p>
         </div>
     </Layout>
 )
